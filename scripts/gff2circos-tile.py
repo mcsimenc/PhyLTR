578fb661b1a1d09ea81def6669663a0207b37213
Matt Simenc <mcsimenc@gmail.com>
2018-02-15 06:18:21 -0800

    updated gff2circos-tile.py -valueDef flag to include LTR.

--- scripts/gff2circos-tile.py
+++ scripts/gff2circos-tile.py
@@ -4,6 +4,56 @@
 
 import sys
 
+class GFF3_line:
+	'''
+	Attributes:
+			field0, ... , field8 - string
+			attributes - dictionary
+
+	Methods:    str()		prints gff line
+		    refreshAttrStr()	updates gff line attributes (needed if changes to attributes were made)
+
+	kwargs is a dictionary
+	'''
+	def __init__(self, line=None, **kwargs):
+
+		if line == None:
+			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = [None]*9
+			self.attributes_order = []
+			self.attributes = {}
+			
+		else:
+			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = line.strip().split('\t')
+			self.start = int(self.start)
+			self.end = int(self.end)
+			attributes_list = self.attributes_str.split(';')
+			self.attributes_order = [ attr.split('=')[0] for attr in attributes_list ]
+			self.attributes = { attr.split('=')[0]:attr.split('=')[1] for attr in attributes_list }
+
+		self.line_number = None
+
+		if 'line_number' in kwargs:	
+			self.line_number = kwargs['line_number']
+
+		# rename the name attribute so it conforms to GFF3 specifications, where Name is a reserved attribute key. The version of LTRDigest makes attribute key name
+		if 'name' in self.attributes:
+			self.attributes['Name'] = self.attributes.pop('name')
+			self.attributes_order[self.attributes_order.index('name')] = 'Name'
+
+	def __repr__(self): # for when str() or repr() are called
+
+		return '\t'.join( [ str(self.seqid), str(self.source), str(self.type), str(self.start), str(self.end), str(self.score), str(self.strand), str(self.phase), str(self.attributes_str) ] )
+
+	def refreshAttrStr(self, attrOrder=None):
+		'''
+		If the attributes have been changed this needs to be called
+		before the changes are reflected on a str() call on this object.
+
+		If an attribute has been added it should have also been added to self.attributes_order
+		'''
+		self.attributes_str = ';'.join([ '='.join([ attr, self.attributes[attr] ]) for attr in self.attributes_order ])
+
+
 
 def help():
 	print('''
@@ -31,22 +81,35 @@ def help():
 					col is the value to assign if the string is found.
 					The second col needs to contain numbers only.
 
+					If valueDef == 'LTR' then the ID attribute is expected to be
+						LTR_retrotransposonN where N is some integer. N is used
+						as the value.
+
 		Output:
 		------------
 		scaf	start	stop	value
 
 ''', file=sys.stderr)
-
 def gff2circosTileTrack(gffFl, valueDef=None):
+
+
 	with open(gffFl) as fl:
 		for line in fl:
 			if not line.startswith('#'):
-				fields = line.strip().split('\t')
-				seq = fields[0]
-				start = fields[3]
-				end = fields[4]
-				value = 0
-				if not valueDef == None:
+				if valueDef=='LTR':
+					gffLine = GFF3_line(line)
+					start = gffLine.start
+					end = gffLine.end
+					value = gffLine.attributes['ID'][19:]
+
+				else:
+					fields = line.strip().split('\t')
+					seq = fields[0]
+					start = fields[3]
+					end = fields[4]
+					value = 0
+
+				if not valueDef == None and not valueDef == 'LTR':
 					FOUND_DEF = False
 					MatchValue = 0
 					for definition in valueDef:
@@ -78,7 +141,10 @@ gff_filepath = args[args.index('-gff') +1]
 
 if '-valueDef' in args:
 	valueDefFl = args[args.index('-valueDef') +1]
-	valueDef = set([ (definition.split('\t')[0], float(definition.split('\t')[1])) for definition in open(valueDefFl).read().strip().split('\n') ])
+	if valueDefFl == 'LTR':
+		valueDef = 'LTR'
+	else:
+		valueDef = set([ (definition.split('\t')[0], float(definition.split('\t')[1])) for definition in open(valueDefFl).read().strip().split('\n') ])
 else:
 	valueDef = None
 
