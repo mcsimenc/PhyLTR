058426c58d8c3835a38da5474e69659313bbdd20
Matt Simenc <mcsimenc@gmail.com>
2018-08-25 10:26:16 -0700

    bugfix: connect -soloLTRsearch as process switch

--- PhyLTR.py
+++ PhyLTR.py
@@ -5933,11 +5933,12 @@ if USEMCL:
 #
 #
 #  
-if WICKER:
-	#summarizeClusters(I=6, clustering_method='WickerFam', WickerParams={'pId':80,'percAln':80,'minLen':80})
-	SoloLTRsearch(I=6, clustering_method='WickerFam', WickerParams={'pId':80,'percAln':80,'minLen':80})
-if USEMCL:
-	SoloLTRsearch(I=MCL_I, clustering_method='MCL', WickerParams={'pId':80,'percAln':80,'minLen':80})
+if SOLOLTR:
+	if WICKER:
+		#summarizeClusters(I=6, clustering_method='WickerFam', WickerParams={'pId':80,'percAln':80,'minLen':80})
+		SoloLTRsearch(I=6, clustering_method='WickerFam', WickerParams={'pId':80,'percAln':80,'minLen':80})
+	if USEMCL:
+		SoloLTRsearch(I=MCL_I, clustering_method='MCL', WickerParams={'pId':80,'percAln':80,'minLen':80})
 
 if CIRCOS:
 	if WICKER:
