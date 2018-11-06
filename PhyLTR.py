06a3a125e30a86e58d3ba4911951477d9fe0f9f2
Mathew Simenc - Biology Student <mcsimenc@kepler.fullerton.edu>
2018-11-06 12:40:01 -0800

    improvement: added add/remove attributes methods for class GFF3_line

--- PhyLTR.py
+++ PhyLTR.py
@@ -17,24 +17,21 @@ from inspect import currentframe, getframeinfo
 class GFF3_line:
 	'''
 	Attributes:
-			seqid, source, type, start, end, score, strand, phase, attributes
+			field0, ... , field8 - string
+			attributes - dictionary
 
-			attributes_order: A list declaring the order of attributes desired.
-					  If new attributes are added to object, they must also
-					  be added to attributs_order and then run refreshAttrStr()
+	Methods:    str()		prints gff line
+		    refreshAttrStr()	updates gff line attributes (needed if changes to attributes were made)
 
-			kwargs: line_number
-
-	Methods:
-			str()			Prints gff line
-			
-			refreshAttrStr()	Updates gff line attributes (needed if changes to attributes were made)
+	kwargs is a dictionary
 	'''
 	def __init__(self, line=None, **kwargs):
+
 		if line == None:
 			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = [None]*9
 			self.attributes_order = []
 			self.attributes = {}
+			
 		else:
 			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = line.strip().split('\t')
 			self.start = int(self.start)
@@ -42,7 +39,9 @@ class GFF3_line:
 			attributes_list = self.attributes_str.split(';')
 			self.attributes_order = [ attr.split('=')[0] for attr in attributes_list ]
 			self.attributes = { attr.split('=')[0]:attr.split('=')[1] for attr in attributes_list }
+
 		self.line_number = None
+
 		if 'line_number' in kwargs:	
 			self.line_number = kwargs['line_number']
 
@@ -52,7 +51,37 @@ class GFF3_line:
 			self.attributes_order[self.attributes_order.index('name')] = 'Name'
 
 	def __repr__(self): # for when str() or repr() are called
+
 		return '\t'.join( [ str(self.seqid), str(self.source), str(self.type), str(self.start), str(self.end), str(self.score), str(self.strand), str(self.phase), str(self.attributes_str) ] )
+
+	def addAttr(self, attr_key, attr_val, replace=False, attr_pos=0):
+		'''
+		Adds attribute, default is at the start of the list.
+		Default behavior is to add attr_val to a growing
+		comma-sep list if attr_key exists. Set replace=True to
+		replace existing attr_val.
+		'''
+		if attr_key in self.attributes:
+			if replace:
+				delAttr(attr_key)
+				self.attributes[attr_key] = attr_val
+				self.attributes_order.insert(attr_pos, attr_key)
+				self.refreshAttrStr()
+			else: # grow list
+				self.attributes[attr_key] = '{0},{1}'.format(self.attributes[attr_key], attr_val)
+				self.refreshAttrStr()
+		else:
+			self.attributes[attr_key] = attr_val
+			self.attributes_order.insert(attr_pos, attr_key)
+			self.refreshAttrStr()
+
+	def delAttr(self, attr_key):
+		'''
+		Deletes attribute.
+		'''
+		del self.attributes[attr_key]
+		self.attributes_order.pop(self.attributes_order.index(attr_key))
+		self.refreshAttrStr()
 
 	def refreshAttrStr(self, attrOrder=None):
 		'''
@@ -62,6 +91,55 @@ class GFF3_line:
 		If an attribute has been added it should have also been added to self.attributes_order
 		'''
 		self.attributes_str = ';'.join([ '='.join([ attr, self.attributes[attr] ]) for attr in self.attributes_order ])
+
+#class GFF3_line:
+#	'''
+#	Attributes:
+#			seqid, source, type, start, end, score, strand, phase, attributes
+#
+#			attributes_order: A list declaring the order of attributes desired.
+#					  If new attributes are added to object, they must also
+#					  be added to attributs_order and then run refreshAttrStr()
+#
+#			kwargs: line_number
+#
+#	Methods:
+#			str()			Prints gff line
+#			
+#			refreshAttrStr()	Updates gff line attributes (needed if changes to attributes were made)
+#	'''
+#	def __init__(self, line=None, **kwargs):
+#		if line == None:
+#			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = [None]*9
+#			self.attributes_order = []
+#			self.attributes = {}
+#		else:
+#			(self.seqid, self.source, self.type, self.start, self.end, self.score, self.strand, self.phase, self.attributes_str) = line.strip().split('\t')
+#			self.start = int(self.start)
+#			self.end = int(self.end)
+#			attributes_list = self.attributes_str.split(';')
+#			self.attributes_order = [ attr.split('=')[0] for attr in attributes_list ]
+#			self.attributes = { attr.split('=')[0]:attr.split('=')[1] for attr in attributes_list }
+#		self.line_number = None
+#		if 'line_number' in kwargs:	
+#			self.line_number = kwargs['line_number']
+#
+#		# rename the name attribute so it conforms to GFF3 specifications, where Name is a reserved attribute key. The version of LTRDigest makes attribute key name
+#		if 'name' in self.attributes:
+#			self.attributes['Name'] = self.attributes.pop('name')
+#			self.attributes_order[self.attributes_order.index('name')] = 'Name'
+#
+#	def __repr__(self): # for when str() or repr() are called
+#		return '\t'.join( [ str(self.seqid), str(self.source), str(self.type), str(self.start), str(self.end), str(self.score), str(self.strand), str(self.phase), str(self.attributes_str) ] )
+#
+#	def refreshAttrStr(self, attrOrder=None):
+#		'''
+#		If the attributes have been changed this needs to be called
+#		before the changes are reflected on a str() call on this object.
+#
+#		If an attribute has been added it should have also been added to self.attributes_order
+#		'''
+#		self.attributes_str = ';'.join([ '='.join([ attr, self.attributes[attr] ]) for attr in self.attributes_order ])
 
 def rename_fasta_seq_headers(in_flpath, in_header_pattern, header_to_name_map, out_flpath):
 	'''
