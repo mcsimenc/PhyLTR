5b1afe2d8f6728983025849b13b0624e1e1e50eb
mcsimenc <mcsimenc@gmail.com>
2019-03-13 11:33:03 -0700

    bugfix: bestBlast.py

--- scripts/bestBlast.py
+++ scripts/bestBlast.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
-
 # This script takes a blast output format 7 table on stdin and prints the first (presumably highest scoring/lowest evalue) hit for each query on stdout
 
-
 import sys
+
+if '-h' in sys.argv:
+	print('''
+ usage:
+	python3 bestBlast.py < blastTabularOutput
+
+ description:
+	this script outputs the identity of the first match for each
+   	query in the output (which is the highest scoring match in
+	blast outfmt 6 or 7) and prints it.
+''', file=sys.stderr)
+	sys.exit()
 
 last_query = '!@#--sjsjsj-BI29(*YFen230*012kr3jnr)FU)R(u$%^&*()_QWERTYUIOP{'
 for line in sys.stdin:
@@ -12,4 +22,3 @@ for line in sys.stdin:
 		fields = line.strip().split('\t')
 		print('\t'.join(fields[:2]))
 		last_query = line.strip().split()[0]
-
