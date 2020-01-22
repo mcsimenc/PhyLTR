3d0834511bddf4654384f72fd78c908928679804
Matt Simenc <mcsimenc@gmail.com>
2020-01-22 08:45:23 -0800

    add inline documentation for script

--- scripts/DfamExtractLTRelements.py
+++ scripts/DfamExtractLTRelements.py
@@ -2,22 +2,32 @@
 
 import sys
 
+
+# this script takes the Dfam multi-nhmm as input and outputs a multi-nhmm file
+# containing only hmm profiles which are annotated as being in the LTR class
 hmmlines = ''
-GETHMM = False
+gethmm = False
 for line in sys.stdin:
+    # recognize line with annotation information and set the variable hmmlines
+    # to true, which signals to output lines of the current profile after they
+    # have all been collected
 	if line.startswith('CT'):
 		if 'Class;' in line:
 			if 'LTR;' in line:
 				hmmlines += line
-				GETHMM = True
+				gethmm = True
 			else:
 				hmmlines = ''
-				GETHMM = False
+				gethmm = False
 	elif line.startswith('//'):
-		if GETHMM:
+        # at the start of the next profile, print the collected lines if the
+        # profile has a LTR annotation, otherwise reset the variable hmmlines
+        # to prepare to collect the next profile's lines
+		if gethmm:
 			hmmlines += line
 			print(hmmlines, end='')
-			GETHMM = False
+			gethmm = False
 		hmmlines = ''
+    # start collecting lines from the start of a profile until the CT line
 	else:
 		hmmlines += line
