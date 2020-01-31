7b12b82ebabbd40eec3539b1c7470af85e926e3d
Matt Simenc <mcsimenc@gmail.com>
2020-01-31 17:25:23 +0000

    Bugfix: Dfam processing scripts outdated

--- scripts/DfamExtractLTRelements.py
+++ scripts/DfamExtractLTRelements.py
@@ -5,19 +5,21 @@ import sys
 hmmlines = ''
 GETHMM = False
 for line in sys.stdin:
-	if line.startswith('CT'):
-		if 'Class;' in line:
-			if 'LTR;' in line:
-				hmmlines += line
-				GETHMM = True
-			else:
-				hmmlines = ''
-				GETHMM = False
-	elif line.startswith('//'):
-		if GETHMM:
-			hmmlines += line
-			print(hmmlines, end='')
-			GETHMM = False
-		hmmlines = ''
-	else:
-		hmmlines += line
+    if line.startswith('CC'):
+        if ' Type:' in line:
+            if 'LTR' in line:
+                hmmlines += line
+                GETHMM = True
+            else:
+                hmmlines = ''
+                GETHMM = False
+        else:
+            hmmlines += line
+    elif line.startswith('//'):
+        if GETHMM:
+            hmmlines += line
+            print(hmmlines, end='')
+            GETHMM = False
+        hmmlines = ''
+    else:
+        hmmlines += line
